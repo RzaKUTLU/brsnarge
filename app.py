0497c88ce624b1b32c79edc1813fcd080186180c
RzaKUTLU <114239738+RzaKUTLU@users.noreply.github.com>
2024-11-03 09:50:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ with col1:
             if st.button("Sipariş Ver") and isim:
                 # Yeni siparişi veritabanına ekle
                 conn.execute('''
-                    INSERT INTO siparisler (tarih, isim, restoran, yemek, fiyat) 
+                    INSERT INTO siparisler (tarih, isim, restoran, yemek, fiyat, not) 
                     VALUES (?, ?, ?, ?, ?)''', 
                     ((datetime.now() + timedelta(hours=3)).strftime("%Y-%m-%d %H:%M"), isim, secilen_restoran, secilen_yemek, fiyat))
                 conn.commit()
